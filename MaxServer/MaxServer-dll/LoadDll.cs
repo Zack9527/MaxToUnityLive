85028381cc434b78bd485dcd2bc5a05076aef260
github <zackzhang163@163.com>
2021-01-11 21:03:07 +0800

    创建vs项目

---
+++ MaxServer/MaxServer-dll/LoadDll.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxServer_dll
+{
+    public class LoadDll
+    {
+        public LoadDll(string path)
+        {
+            Assembly assem = Assembly.LoadFile(path);
+        }
+    }
+}
